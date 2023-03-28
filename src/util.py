13718b887a2b63092e9464e52911f2c60729e296
Hieu Vuong <tvuong5@dons.usfca.edu>
2023-03-28 10:23:44 -0700

    Fix query in util.

--- src/util.py
+++ src/util.py
@@ -1,8 +1,26 @@
-"""Collection of util functions.
+"""
+Collection of util functions.
 
 """
-def generate_league_query(date: str, league:str):
-  return f"SG.DateTime_UTC >= '{date}' AND Tournament='{league}'"
+from typing import List
+
+def generate_league_query(date: str, leagues:List[str]):
+  if len(leagues) == 1:
+    # Only 1 leauge is provided
+    return f"SG.DateTime_UTC >= '{date}' AND Tournament LIKE '%{leagues[0]}%'"
+  else:
+    # loop thru each league and add to the query
+    league_info = ""
+    for index, league in enumerate(leagues):
+      league_info += f"Tournament LIKE '%{league}%'"
+      if index != (len(leagues) - 1):
+        # if not the last item
+        # add the word or
+        league_info += " OR "
+
+    base_query = f"SG.DateTime_UTC >= '{date}' AND ({league_info})"
+
+    return base_query
 
 def generate_general_query(date: str):
   return f"SG.DateTime_UTC >= '{date}'"
