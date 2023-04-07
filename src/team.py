65aa4e11d1c9270732fad771454ec3c624d8f8f0
Hieu Vuong <tvuong5@dons.usfca.edu>
2023-04-07 15:40:11 -0700

    Document

--- src/team.py
+++ src/team.py
@@ -1,5 +1,6 @@
 """
   Add games to team history and get record against opponent team.
+
 """
 
 from src.result import Result
@@ -13,17 +14,22 @@ class Team():
     league: The league that this team belongs to.
 
   """
-  def __init__(self, team_name: str,
-              league: str,
-              history: dict[str, Result]) -> None:
-    """Initializes the Team object given 
-    the league it belongs to,
-    and its scores against other teams.
-    
+  def __init__(
+    self,
+    team_name: str,
+    league: str,
+    history: dict[str, Result]) -> None:
+    """Initializes the Team object.
+
+    Args:
+      team_name: name of the team.
+      league: the league it belongs to 
+      history: team's match history.
     """
     self.team_name = team_name
     self.history = history
     self.league = league
+
 
   def add_game(self, game: Result):
     """Add game to team's history.
@@ -33,15 +39,15 @@ class Team():
 
     If the opponent we've not faced -> set new record against them.
 
+    Only increase the score if we've faced opponent:
+      - of the same league
+      - happen on the same day
+
     Args:
       game: the match to update the team's history with.
     
     """
-    # Add the game and its score.
-    # Destructure the result
-
     if (game.opponent, game.league, game.match_time) in self.history:
-    # increase the score if we win
       self.history[(game.opponent, game.league, game.match_time)] += game.score
     else:
       self.history[(game.opponent, game.league, game.match_time)] = game.score
@@ -59,6 +65,7 @@ class Team():
       Args:
         opponent: the opposition team to look up.
         league: the league that enemy team is in.
+        match_time: time that the match occurred.
 
       Returns:
         Result object that contains opponent's name
@@ -69,6 +76,5 @@ class Team():
 
   def __str__(self) -> str:
     """A string representation of the result.
-
     """
     return f"{self.team_name} result is {self.history}"
